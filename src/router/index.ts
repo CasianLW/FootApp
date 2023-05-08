f922e3bf08ba768b6cdda185dd2c0a604762d4d3
CasianLW <ciorbacasian@gmail.com>
2023-05-08 22:29:21 +0200

    create empty pages & routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,42 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import Home from "../views/Home.vue";
+import Classement from "../views/Classement.vue";
+import Equipes from "../views/Equipes.vue";
+import Equipe from "../views/Equipe.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '',
-    redirect: '/folder/Inbox'
+    path: "/",
+    component: Home,
   },
   {
-    path: '/folder/:id',
-    component: () => import ('../views/FolderPage.vue')
-  }
-]
+    path: "/ligues/:id/classement",
+    component: Classement,
+  },
+  {
+    path: "/ligues/:id/equipes",
+    component: Equipes,
+  },
+  {
+    path: "/equipes/:id",
+    component: Equipe,
+    name: "equipe",
+  },
+  // Add the following routes from the JavaScript router
+  {
+    path: "",
+    redirect: "/folder/Inbox",
+  },
+  {
+    path: "/folder/:id",
+    component: () => import("../views/FolderPage.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
