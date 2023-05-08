b8693b135e831c5930df50efc0e6b77dee10f51b
CasianLW <ciorbacasian@gmail.com>
2023-05-08 22:58:27 +0200

    Composant pour toggle le menu

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu-container">
+    <div class="menu">
+      <ion-menu-button color="primary"></ion-menu-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuComponent",
+});
+</script>
+
+<style scoped>
+.menu-container {
+  margin-top: 16px;
+  display: flex;
+}
+
+/* Add additional styles for the menu items or other content you want to display */
+</style>
